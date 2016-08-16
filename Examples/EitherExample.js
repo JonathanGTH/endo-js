5198d66d7c1e66de0da8f0ecbfeb6ad1377b751b
Johanan Gonzalez <ImJonathanGth@gmail.com>
2016-08-16 10:28:34 -0400

    Delete EitherExample.js

--- Examples/EitherExample.js
+++
@@ -1,8 +0,0 @@
-var endo = require('../endo'),
-    Either = endo.Either;
-
-var something = new Either.Right(10).chain(function(n){
-    return new Either.Left('Failure: ' + n);
-});
-
-console.log(something.toString()); // Left Failure: 10
